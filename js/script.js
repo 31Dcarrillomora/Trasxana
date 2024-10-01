d5919470b40b50c5fc59c6d31a4e26ca4eae4438
Diego Alexis Carrillo Mora <dcarrillomora31@gmail.com>
2024-10-01 15:47:53 -0500

    Trasxana1

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Puedes agregar cualquier funcionalidad de JavaScript aquí
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("La página está completamente cargada.");
+});
